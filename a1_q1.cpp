c74971bc06dbd046d94c30ce6fb3d4c092c15c75
manasi584 <120204669+manasi584@users.noreply.github.com>
2023-01-14 20:37:25 +0530

    Submitting Q1 of Assignment 1

---
+++ a1_q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+for (int i =0;i<5;i++){
+    cout<<"*";
+}
+cout<<endl;
+cout<<"   *"<<endl;
+cout<<"  *"<<endl;
+cout<<" *"<<endl;
+for (int i =0;i<5;i++){
+    cout<<"*";
+}
+
+    return 0;
+}
